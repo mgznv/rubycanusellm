258a2590631277cd58a54ff1490848edcb22777c
Juan Manuel Guzman Nava <tu-email-personal>
2026-03-31 14:21:59 -0600

    Initial gem structure via bundle gem

---
+++ lib/rubycanusellm.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "rubycanusellm/version"
+
+module Rubycanusellm
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/rubycanusellm_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Rubycanusellm do
+  it "has a version number" do
+    expect(Rubycanusellm::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
